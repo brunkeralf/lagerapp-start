557c982806a32c96f39429e4207f840fe41537fd
brunkeralf <brunkeralf@gmail.com>
2025-07-28 10:59:18 +0200

    Create SearchScreen.js

---
+++ SearchScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Här söker du i lagret.</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
