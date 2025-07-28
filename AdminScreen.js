209bfa80fcf0a25e2cd14a229135e92c13e9c343
brunkeralf <brunkeralf@gmail.com>
2025-07-28 11:01:20 +0200

    Create AdminScreen.js

---
+++ AdminScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AdminScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Inställningar och adminpanel.</Text>
+    </View>
+  );
+};
+
+export default AdminScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
