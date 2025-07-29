4b158f40a13452c7a693c6455070a533b70c8718
brunkeralf <brunkeralf@gmail.com>
2025-07-29 13:29:46 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import HomeScreen from './HomeScreen';
 import SearchScreen from './SearchScreen';
 import WarehouseScreen from './WarehouseScreen';
-import AdminScreen from './AdminScreen';
 import CompartmentScreen from './CompartmentScreen';
-const Stack = createNativeStackNavigator();
 import AddArticleScreen from './AddArticleScreen';
+import AdminScreen from './AdminScreen';
+import RetrieveArticleScreen from './RetrieveArticleScreen'; // ← Ny!
+
+const Stack = createStackNavigator();
+
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={HomeScreen} />
-        <Stack.Screen name="Sök lager" component={SearchScreen} />
-        <Stack.Screen name="Gå till lager" component={WarehouseScreen} />
-        <Stack.Screen name="Inställningar/Admin" component={AdminScreen} />
-<Stack.Screen name="Compartment" component={CompartmentScreen} />
-<Stack.Screen name="Lägg till artikel" component={AddArticleScreen} />      </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Warehouse" component={WarehouseScreen} />
+        <Stack.Screen name="Compartment" component={CompartmentScreen} />
+        <Stack.Screen name="AddArticle" component={AddArticleScreen} />
+        <Stack.Screen name="Admin" component={AdminScreen} />
+        <Stack.Screen name="RetrieveArticle" component={RetrieveArticleScreen} /> {/* ← Ny! */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
