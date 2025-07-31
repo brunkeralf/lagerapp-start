3ba12d1ee8428976b1eafeda2865ef23639c57ca
brunkeralf <brunkeralf@gmail.com>
2025-07-31 21:49:19 +0200

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,2 +1,13 @@
 @echo off
-echo This is a placeholder gradlew.bat script for GitHub Actions
+REM ##########################################################################
+REM
+REM  Gradle start up script for Windows
+REM
+REM ##########################################################################
+
+set DIR=%~dp0
+set APP_BASE_NAME=%~nx0
+
+set DEFAULT_JVM_OPTS=
+
+"%DIR%gradle\wrapper\gradle-wrapper.jar" %*
