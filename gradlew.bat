27c52d30da257592b9d3b629ad350c7ed843e736
brunkeralf <brunkeralf@gmail.com>
2025-08-01 09:32:17 +0200

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,13 @@
+@echo off
+REM ##########################################################################
+REM
+REM  Gradle start up script for Windows
+REM
+REM ##########################################################################
+
+set DIR=%~dp0
+set APP_BASE_NAME=%~nx0
+
+set DEFAULT_JVM_OPTS=
+
+"%DIR%gradle\wrapper\gradle-wrapper.jar" %*
